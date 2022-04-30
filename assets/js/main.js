ccc6d81fd2e90b2845dbb4095a32a012326ac657
uyilboga <ylbogaufuk@gmail.com>
2022-04-30 16:49:08 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$(function() {
+
+	// Header Scroll Active
+	$(window).scroll(function() {
+        $(window).scrollTop() > 100 ? $("header").addClass("scroll-active") : $("header").removeClass("scroll-active")
+    }),
+    $(document).ready(function() {
+        $(window).scrollTop() > 100 ? $("header").addClass("scroll-active") : $("header").removeClass("scroll-active")
+    });
+
+	// Mobile Menü
+	$(".mobile-btn").on('click', function() {
+		$(".mobile-menu").addClass('active');
+		$('body').addClass('overflow-hidden');
+		setTimeout(function() {
+			$(".mobile-menu-main").addClass('active');
+		}, 300);
+	})
+
+	$(".mobile-close-btn, .mobile-menu .overlay").on('click', function() {
+		$(".mobile-menu-main").removeClass('active');
+		setTimeout(function() {
+			$(".mobile-menu").removeClass('active');
+			$('body').removeClass('overflow-hidden');
+		}, 500);
+	})
+
+	$(".mobile-menu .menu ul li a.has-child").on('click', function() {
+		$(this).parent().find('ul').slideToggle();
+		$(this).toggleClass('active');
+
+		$(this).parent().siblings().find('ul').slideUp();
+		$(this).parent().siblings().find('a').removeClass('active');
+	})
+});
